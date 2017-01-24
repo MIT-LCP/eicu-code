7e04cb475d7b07cbe2352e925734fcf1ff634701
Tom Pollard <tpollard@mit.edu>
2017-01-23 19:32:15 -0500

    tidying

--- concepts/basic_demographics.sql
+++ concepts/basic_demographics.sql
@@ -1,16 +1,19 @@
 -- ------------------------------------------------------------------  
 -- Title: Basic Demographics
--- Description: Extracts patient unique identifier, gender, age, admission diagnosis, hospital mortality and icu length of stay.
+-- Description: Extracts patient unique identifier, gender, age, 
+--              admission diagnosis, hospital mortality and 
+--              icu length of stay.
 -- Matthieu Komorowski, LCP MIT, 12/23/2016
--- eICU-CRD v1.0
+-- eICU-CRD v0.1
 -- ------------------------------------------------------------------  
 
-select pt.patientunitstayid,
-(case when pt.gender = 'Male' then 1 when pt.gender='Female' then 2 else null end) as gender,
-pt.age,
-pt.apacheadmissiondx,
-(case When pt.hospitaldischargestatus = 'Alive' Then 0
-when pt.hospitaldischargestatus = 'Expired' Then 1 else Null End ) as hospital_mortality,
-round(pt.unitdischargeoffset/60) as icu_los_hours
-from eicu.patient pt
-order by pt.patientunitstayid
+SELECT pt.patientunitstayid, pt.age, pt.apacheadmissiondx, 
+       CASE WHEN pt.gender = 'Male' THEN 1 
+            WHEN pt.gender = 'Female' THEN 2 
+            ELSE NULL END) AS gender,
+       CASE WHEN pt.hospitaldischargestatus = 'Alive' THEN 0
+            WHEN pt.hospitaldischargestatus = 'Expired' THEN 1 
+            ELSE NULL END) AS hosp_mortality,
+       ROUND(pt.unitdischargeoffset/60) AS icu_los_hours
+FROM eicu.patient pt
+ORDER BY pt.patientunitstayid;
