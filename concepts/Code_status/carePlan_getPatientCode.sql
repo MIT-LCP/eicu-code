5349bfe58c20a3965d27877fae8266fa0c55dfbd
psy01212 <34636332+psy01212@users.noreply.github.com>
2018-03-02 13:47:54 -0500

    fixed semicolon errors

--- concepts/Code_status/carePlan_getPatientCode.sql
+++ concepts/Code_status/carePlan_getPatientCode.sql
@@ -1,6 +1,6 @@
 -- NOT SUPPORTED
 
-DROP MATERIALIZED VIEW IF EXISTS patientCodeStatus
+DROP MATERIALIZED VIEW IF EXISTS patientCodeStatus;
 CREATE MATERIALIZED VIEW patientCodeStatus AS
 
 SELECT "patientunitstayid", "cplitemoffset", string_agg("cplitemvalue", ', ') AS "cplitemvalue"
@@ -10,4 +10,4 @@ FROM (SELECT *
 FROM "careplangeneral"
 WHERE ("cplitemvalue" IN ('Full therapy', 'Do not resuscitate', 'No CPR', 'No intubation', 'Comfort measures only', 'No cardioversion', 'No vasopressors/inotropes', 'No augmentation of care', 'End of life', 'No blood products', 'No blood draws', 'Advance directives'))) "ivtmaaxwzk") "oytldaespk"
 ORDER BY "patientunitstayid", "cplitemoffset", "cplitemvalue") "vrxthajevr"
-GROUP BY "patientunitstayid", "cplitemoffset"
+GROUP BY "patientunitstayid", "cplitemoffset";
