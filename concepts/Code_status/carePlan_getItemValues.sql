a26dab32d386b16dbbebdeed40620f5c79141727
psy01212 <34636332+psy01212@users.noreply.github.com>
2018-03-02 13:54:25 -0500

    getItemValues.sql creates a materialized view

--- concepts/Code_status/carePlan_getItemValues.sql
+++ concepts/Code_status/carePlan_getItemValues.sql
@@ -1,6 +1,9 @@
 -- NOT SUPPORTED
 
+DROP MATERIALIZED VIEW IF EXISTS possibleCodeStatus;
+CREATE MATERIALIZED VIEW possibleCodeStatus AS
+
 SELECT "cplitemvalue", count(*) AS "n"
 FROM "careplangeneral"
 GROUP BY "cplitemvalue"
-ORDER BY "n" DESC
+ORDER BY "n" DESC;
